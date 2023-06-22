8968703ba5d616655f78be04dd7030b9f48de293
brunodias77 <brunohdias95@gmail.com>
2023-06-22 18:22:35 -0300

    frist commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop2.Controllers
+{
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public string MeuMetodoGet()
+        {
+            return "Voce requisitou um metodo get na api do bruno dias";
+        }
+        [HttpPost]
+        [Route("")]
+        public string MeuMetodoPost()
+        {
+            return "Voce requisitou um metodo Post";
+        }
+    }
+}
+
